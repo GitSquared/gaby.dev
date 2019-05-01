d56595d6a48aa64e7dae71be85c2a29bba155630
Gabriel SAILLARD (GitSquared) <gabriel@saillard.dev>
2019-05-01 14:53:17 +0200

    Import

---
+++ index.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+
+router.get('/', ctx => {
+	ctx.redirect('https://squared.codebrew.fr');
+});
+
+router.get('/live', ctx => {
+	ctx.redirect('https://www.youtube.com/channel/UCiVqJ8VrvjqRKuPD_Hj-6bw');
+});
+
+app.use(async (ctx, next) => {
+		const start = Date.now();
+		await next();
+		const ms = Date.now() - start;
+		ctx.set('X-Response-Time', `${ms}ms`);
+	}).use(router.routes())
+	.use(router.allowedMethods());
+
+module.exports = app.callback();
