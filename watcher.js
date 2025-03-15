a0f082064f39db13583d2c9e34cd4e770c2dadcf
Gabriel Saillard <gabriel@saillard.dev>
2025-03-15 14:31:59 +0100

    chore: clean up dead blog and other stuff

--- watcher.js
+++
@@ -1,8 +0,0 @@
-const { spawn } = require('child_process');
-const chokidar = require('chokidar');
-
-chokidar.watch('public/posts/').on('all', (_, path) => {
-	if (!path.endsWith('index.md')) return
-	console.log(path)
-	spawn('npm', ['run', 'build'])
-})
